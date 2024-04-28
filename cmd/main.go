0a4ad441f08c36a8bff2898812d160d3884fa12f
bartukocakara <kocakarabartu@gmail.com>
2024-04-28 23:32:11 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/bartukocakara/grpc-shopping-order/client"
+	"github.com/bartukocakara/grpc-shopping-order/config"
+	"github.com/bartukocakara/grpc-shopping-order/db"
+	"github.com/bartukocakara/grpc-shopping-order/pb"
+	"github.com/bartukocakara/grpc-shopping-order/service"
+	"google.golang.org/grpc"
+)
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	h := db.Init(c.DBUrl)
+
+	lis, err := net.Listen("tcp", c.Port)
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	productSvc := client.InitProductServiceClient(c.ProductSvcUrl)
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	fmt.Println("Order Svc on", c.Port)
+
+	s := service.Server{
+		H:          h,
+		ProductSvc: productSvc,
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterOrderServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
+}
